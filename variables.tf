e1e2da06379137d0fd1e7ec5430b13ce9d72e67c
ecsd-matthew-song <44193019+ecsd-matthew-song@users.noreply.github.com>
2020-02-14 09:48:01 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -73,3 +73,13 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "is_billable" {
+  default     = "yes"
+  description = "the resource is billable"
+}
+
+variable "department" {
+  default     = "devops"
+  description = "Department"
+}
